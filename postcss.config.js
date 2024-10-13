f833e9b696c038c3678f143e52613e2ac1741953
CHENYUQING <chen@appurunoMacBook-Pro.local>
2024-10-13 20:00:34 +0900

    footerの美化

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,4 @@
-module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('postcss-flexbugs-fixes'),
-    require('tailwindcss'),  // 添加 Tailwind CSS 插件
-    require('autoprefixer'), // 添加 Autoprefixer 插件
-    require('postcss-preset-env')({
-      autoprefixer: {
-        flexbox: 'no-2009'
-      },
-      stage: 3
-    })
-  ]
-}
+export const plugins = {
+  tailwindcss: {},
+  autoprefixer: {},
+};
