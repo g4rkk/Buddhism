7c4425059a439357180ac6da1f72b030cd9baf21
CHENYUQING <chen@appurunoMacBook-Pro.local>
2024-10-15 01:54:01 +0900

    瞑想ガイドの選択機能

--- app/models/meditation_guide.rb
+++ app/models/meditation_guide.rb
@@ -1,4 +1,16 @@
 class MeditationGuide < ApplicationRecord
   has_one_attached :audio
   has_many :meditation_sessions
+
+  # 在保存之前自动生成 slug
+  before_save :set_slug
+
+  private
+
+  # 如果 slug 为空，则根据 title 自动生成 slug
+  def set_slug
+    if slug.blank?
+      self.slug = title.parameterize
+    end
+  end
 end
