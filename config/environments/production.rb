75213c6c5611a3548426de4597f6103fdf1f98ff
CHENYUQING <chen@appurunoMacBook-Pro.local>
2024-10-15 07:16:06 +0900

    デプロイ3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,9 +38,9 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
   # Store uploaded files in Tigris Global Object Storage (see config/storage.yml for options).
-  config.active_storage.service = :tigris
+  config.active_storage.service = :local
 
-  # Mount Action Cable outside main process or domain.
+  #Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
