8189fd33d225393c6bc45dfc00a7817b355532f9
CHENYUQING <chen@appurunoMacBook-Pro.local>
2024-10-21 17:02:12 +0900

    dbの修正

--- db/migrate/20241020165443_add_user_to_books.rb
+++ db/migrate/20241020165443_add_user_to_books.rb
@@ -2,9 +2,12 @@ class AddUserToBooks < ActiveRecord::Migration[7.0]
   def change
     # 找到管理员用户
     admin = User.find_by(email: 'ceciliachen721@gmail.com')
-    
-    # 添加 user_id 字段，允许为 null
-    add_reference :books, :user, foreign_key: true, null: true
+
+    # 检查 books 表中是否已经有 user_id 列
+    unless column_exists?(:books, :user_id)
+      # 添加 user_id 字段，允许为 null
+      add_reference :books, :user, foreign_key: true, null: true
+    end
 
     # 将已有的书籍都关联到管理员
     Book.where(user_id: nil).update_all(user_id: admin.id)
