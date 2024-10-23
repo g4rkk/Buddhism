a4b567d6173190e76f6833156a3ef3e7755ff647
CHENYUQING <chen@appurunoMacBook-Pro.local>
2024-10-23 14:17:35 +0900

    google認証の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   # Devise 模块
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
 
   # 默认用户不是管理员
   attribute :admin, :boolean, default: false
@@ -16,4 +17,20 @@ class User < ApplicationRecord
   # 书签和书籍的关联
   has_many :bookmarks
   has_many :bookmarked_books, through: :bookmarks, source: :book
+
+  # 从 OmniAuth 返回的数据中查找或创建用户
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
+
+    # 如果用户不存在，则创建新用户
+    unless user
+      user = User.create(
+        username: data['name'],
+        email: data['email'],
+        password: Devise.friendly_token[0, 20]
+      )
+    end
+    user
+  end
 end
